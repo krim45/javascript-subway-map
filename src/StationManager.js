49ed4c7b16bb1e381319af47d89eb938eb08f935
krim45 <krim45@naver.com>
2021-11-19 21:16:47 +0900

    feat: 지하철 역 추가 시 역 이름 유효성 검사 > 역 이름 두 글자 이상 > 역 이름 중복 시 추가 등록할 수 없음

--- src/StationManager.js
+++ src/StationManager.js
@@ -1,31 +1,56 @@
-export class StationManager {
-  // localStorage도 사용해야 한다.
+class StationManager {
   constructor() {
     this.station = [];
-    this.newListId = 0;
+    this.newListId = 1;
+    this.stationList = document.querySelector('#station-add-list');
+    this.stationNameInput = document.querySelector('#station-name-input');
   }
 
-  namedStation() {
-
-
+  isValid(str) {
+    let isContain = false;
+    this.station.forEach(item => {
+      if (item.name === str) isContain = true;
+    })
+    return str.length > 1 && !isContain
   }
 
-  addList() {
-    const stationList = document.querySelector('#station-add-list');
+  giveWarning() {
+    alert('이미 존재하는 역 입니다.')
+    this.clearAll();
+  }
+
+  makeTable() {
     const tr = document.createElement('tr');
     const tdName = document.createElement('td');
     const tdWrap = document.createElement('td');
     const tdDelBtn = document.createElement('input');
-    const stationName = document.querySelector('#station-name-input').value;
+    tdName.textContent = this.stationNameInput.value;
 
-    tdName.textContent = stationName;
-    // tdDelBtn.setAttribute('class', 'delete-list');
+    tdDelBtn.setAttribute('class', 'delete-list');
     tdDelBtn.setAttribute('type', 'button');
     tdDelBtn.setAttribute('value', '삭제');
-    tdDelBtn.addEventListener('click', this.deleteList)
     tdWrap.append(tdDelBtn);
     tr.append(tdName, tdWrap);
-    stationList.append(tr);
+    this.stationList.append(tr);
+
+    tdDelBtn.addEventListener('click', this.deleteList.bind(this));
+  }
+
+  addStation() {
+    if (this.isValid(this.stationNameInput.value)) {
+      this.makeTable();
+      this.station.push({
+        id: this.newListId++,
+        name: this.stationNameInput,
+      })
+      this.clearAll();
+    } else {
+      this.giveWarning();
+    }
+  }
+
+  clearAll() {
+    this.stationNameInput.value = '';
   }
 
   deleteList() {
@@ -35,4 +60,5 @@ export class StationManager {
 
 const stationManager = new StationManager();
 const addBtn = document.querySelector('#station-add-button');
-addBtn.addEventListener('click', stationManager.addList);
+// addBtn.addEventListener('click', stationManager.addStation);
+addBtn.addEventListener('click', stationManager.addStation.bind(stationManager));
